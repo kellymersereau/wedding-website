5d9c4e4415f24e550858696dad551bcbca9e6016
Kelly M <kmersereau@besentient.com>
2018-09-25 15:41:29 -0400

    updating 404 page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,1 +1,13 @@
 
+@extends('template')
+
+@section('content')
+	
+	<div class="placeholder-hero">
+		<h1>
+			Check back soon for information about our wedding. <img src="/img/icons/heart-icon.png">
+		</h1>
+	</div>
+
+
+@endsection
---
+++ resources/views/pages/plans.blade.php
@@ -0,0 +1,12 @@
+@extends('template')
+
+@section('content')
+	
+	<div class="placeholder-hero">
+		<h1>
+			Check back soon for information about our wedding. <img src="/img/icons/heart-icon.png">
+		</h1>
+	</div>
+
+
+@endsection
