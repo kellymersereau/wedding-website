6bcede7356e6ef6ce0409d916b33efec19b0aa11
Kelly M <kmersereau@besentient.com>
2018-09-25 15:51:15 -0400

    updating 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 	
 	<div class="placeholder-hero">
-		<h1>{{ $exception->getStatusCode() }}</h1>
+		<h1 class="error-code">{{ $exception->getStatusCode() }}</h1>
 		<h2>Sorry &mdash; We're unable to find the page you are looking for.</h2>
 		<a href="/" title="Home">Go back.</a>
 	</div>
