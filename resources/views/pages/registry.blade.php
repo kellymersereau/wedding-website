b053673e23e00e5e3af46ccfb79ebd0981812ac1
Kelly M <kmersereau@besentient.com>
2018-11-04 15:20:52 -0500

    updating site

--- resources/views/pages/registry.blade.php
+++ resources/views/pages/registry.blade.php
@@ -2,11 +2,30 @@
 
 @section('content')
 	
-	<div class="placeholder-hero">
-		<h1>
-			Coming soon...
-		</h1>
+	<div class="hero">
+		<div class="clipped-title">
+			<h1>
+				Registry
+			</h1>
+		</div>
 	</div>
 
+	<div class="wayfair">
+		<a href="https://www.wayfair.com/registry/wedding/KellyAndBrian10.11.2019" target="_blank" norelopener>
+			<img src="/img/icons/wayfair-logo.png" alt="Wayfair Logo">
+		</a>
+	</div>
+	
+	<div class="crate-and-barrel">
+		<a href="https://www.crateandbarrel.com/gift-registry/kelly-mersereau-and-brian-taylor/r5892057" target="_blank" relnoopener>
+			<img src="/img/icons/Crate-Barrel-Logo.png" alt="Crate and Barrel Logo">
+		</a>
+	</div>
+	
+	<div class="amazon">
+		<a href="https://www.amazon.com/gp/registry/wedding/9OPF6B7IEA5U" target="_blank" norelopener>
+			<img src="/img/icons/amazon_logo_RGB.jpg" alt="Amazon Logo">
+		</a>
+	</div>
 
 @endsection
